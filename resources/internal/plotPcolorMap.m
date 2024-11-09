aad00aa62595f197871ddf3714924d21cbfbd09d
annarufas <128830450+annarufas@users.noreply.github.com>
2024-11-09 14:58:59 +0000

    Update comments

--- resources/internal/plotPcolorMap.m
+++ resources/internal/plotPcolorMap.m
@@ -1,16 +1,18 @@
 function plotPcolorMap(haxis,lonVector,latVector,myData,myColormap,caxisMin,caxisMax)
 
+    % Set projection
     m_proj('Robinson', 'clongitude', -63);
-    
+
+    % Swap latitude and longitude so that nRows = nLat and nCols = nLon
     [nRows,nCols] = size(myData);
     if (nRows > nCols)
-        myData = flipud(rot90(myData));
+        myData = permute(myData, [2, 1]);
     end
-    
+
     myData(myData == Inf) = 0;
-%     max(max(myData))
-%     minValue = min(myData(myData > 0))
-    
+
+    % Because of the quirks of flat pcolor, there is a white line in the
+    % join -180 to 180 (all the global projections have 360 deg ambiguities)
     m_pcolor(lonVector,latVector,myData);
     if any(lonVector(:) < 0)
         hold on;
@@ -18,9 +20,10 @@ function plotPcolorMap(haxis,lonVector,latVector,myData,myColormap,caxisMin,caxi
     end
     
     caxis([caxisMin caxisMax]);
-    shading flat;
+%     shading flat;
     colormap(haxis,myColormap);
-    m_grid('xticklabels', [], 'yticklabels', [], 'ticklen', 0, 'linestyle', 'none');
+
+    m_grid('xtick', [-180:60:180], 'ytick', [-90:30:90], 'xticklabels', [], 'yticklabels', []); % , 'ticklen', 0.1, 'linestyle', 'none'
     m_coast('patch', [.7 .7 .7], 'edgecolor', 'black');
         
 end % plotPcolorMap
