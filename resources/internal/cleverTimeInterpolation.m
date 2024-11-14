0e9224ec049750d536cd3d59b08534f9da4664e3
annarufas <128830450+annarufas@users.noreply.github.com>
2024-11-14 21:36:29 +0000

    Update functions

--- resources/internal/cleverTimeInterpolation.m
+++ resources/internal/cleverTimeInterpolation.m
@@ -1,24 +1,50 @@
-function [qarray] = cleverTimeInterpolation(array,t,columnCoords,rowCoords)
+function qArray = cleverTimeInterpolation(array,t)
 
-qarray = array;
+% CLEVERTIMEINTERPOLATION This function performs a multi-step interpolation 
+% on a time vector. It is designed for filling gaps in time-series. It uses 
+% three distinct interpolation rounds to handle different types of missing 
+% values: (i) cases where zeros are surrounded by valid data, (ii) cases 
+% where multiple consecutive zeros are surrounded at both sides by valid 
+% data, (iii) cases where zeros are at the beginning or end of the series, 
+% with valid data only on one side (uses "capicúa" or flip-based 
+% interpolation to extrapolate values from the opposite end).
+%
+%   INPUT: 
+%       array  - array with gaps
+%       t      - indices of time vector
+%
+%   OUTPUT:
+%       qArray - array with gaps filled in
+%
+%   WRITTEN BY A. RUFAS, UNIVERISTY OF OXFORD
+%   Anna.RufasBlanco@earth.ox.ac.uk
+%
+%   Version 1.0 - Completed 13 Nov 2024  
+%
+% =========================================================================
+%%
+% -------------------------------------------------------------------------
+% PROCESSING STEPS
+% -------------------------------------------------------------------------
 
-for iCol = 1:length(columnCoords)
-%     disp(iLat)
-    for iRow = 1:length(rowCoords)  
-        
-         arraymonth = squeeze(array(iRow,iCol,:));  
-         
-         if (sum(arraymonth > 0) > 1) % if there have been >= 2 months in time when there was chla, interpolate or extrapolate
+%% Get rid of NaNs
 
-             % First interpolation round: interpolate in bounded spaces,
-             % where a zero value is bounded in both sides
-             
-             matchZeros = (arraymonth==0);
-             nMatchZeros = sum(matchZeros);
-             if (nMatchZeros > 0)
+qArray = array;
+
+for iRow = 1:size(array,1)
+    for iCol = 1:size(array,2)
+         timeSlice = squeeze(array(iRow,iCol,:));  
+
+         if sum(timeSlice > 0) > 1 % if there have been >= 2 months in time when there was chla, interpolate or extrapolate
+
+             % First interpolation round: interpolate zeros between 
+             % non-zero values
+
+             matchZeros = (timeSlice==0);
+             if any(matchZeros)
                  for iMonth = 2:11 % start at second month 
-                     if (arraymonth(iMonth) == 0 && arraymonth(iMonth-1) ~= 0 && arraymonth(iMonth+1) ~= 0)
-                        arraymonth(iMonth) = interp1(t(~matchZeros),arraymonth(~matchZeros),...
+                     if (timeSlice(iMonth) == 0 && timeSlice(iMonth-1) ~= 0 && timeSlice(iMonth+1) ~= 0)
+                        timeSlice(iMonth) = interp1(t(~matchZeros),timeSlice(~matchZeros),...
                             t(iMonth),'linear','extrap');
                      end
                  end
@@ -28,23 +54,20 @@ for iCol = 1:length(columnCoords)
              % where various zero values are bounded in both sides. There
              % can be more than one chunk meeting this condition. Repeat up
              % to four times
- 
+
              for i = 1:4
-                 
-                matchZeros = (arraymonth==0);
-                nMatchZeros = sum(matchZeros);
-                
-                if (nMatchZeros > 0)
+                matchZeros = (timeSlice==0);
+                if any(matchZeros)
 
                      iEndBoundedSpace = 12; % default
 
                      % Search for the beginning and the end of the zero trail
                      for iMonth = 2:11
-                         if (arraymonth(iMonth-1) ~= 0 && arraymonth(iMonth) == 0 && arraymonth(iMonth+1) == 0) 
+                         if (timeSlice(iMonth-1) ~= 0 && timeSlice(iMonth) == 0 && timeSlice(iMonth+1) == 0) 
                              iStartBoundedSpace = iMonth;
                              iEndBoundedSpace = iStartBoundedSpace;
                              for iBound = iStartBoundedSpace+1:12
-                                 if (arraymonth(iBound) == 0)
+                                 if (timeSlice(iBound) == 0)
                                      iEndBoundedSpace = iEndBoundedSpace + 1;
                                  else
                                      break
@@ -57,8 +80,8 @@ for iCol = 1:length(columnCoords)
                      % If the end is not in the last position
                      if (iEndBoundedSpace < 12)
                          nValsToInterp = iEndBoundedSpace - iStartBoundedSpace + 1;
-                         limStart = arraymonth(iStartBoundedSpace-1);
-                         limEnd = arraymonth(iEndBoundedSpace+1);
+                         limStart = timeSlice(iStartBoundedSpace-1);
+                         limEnd = timeSlice(iEndBoundedSpace+1);
                          vectorToInterp = zeros(nValsToInterp+2,1);
                          vectorToInterp(1) = limStart;
                          vectorToInterp(end) = limEnd;
@@ -66,30 +89,28 @@ for iCol = 1:length(columnCoords)
                          tt = 1:nValsToInterp+2;
                          interpVals = interp1(tt(~matchZerosVec),vectorToInterp(~matchZerosVec),...
                              tt(matchZerosVec),'linear','extrap');
-                         arraymonth(iStartBoundedSpace:iEndBoundedSpace) = interpVals;
+                         timeSlice(iStartBoundedSpace:iEndBoundedSpace) = interpVals;
                      end
 
                 else
-                    
+
                     break
-                    
+
                 end
-             
+
              end % number of chunks for repetition
 
              % Third interpolation round: capicua    
-             matchZeros = (arraymonth==0);
-             nMatchZeros = sum(matchZeros);
-             
-             if (nMatchZeros > 0)
+             matchZeros = (timeSlice==0);
+             if any(matchZeros)
 
                  nValsToInterp = sum(matchZeros);
-                 iStartMatchZero = find(arraymonth == 0, 1,'first');
-                 iEndMatchZero = find(arraymonth == 0,1,'last');
+                 iStartMatchZero = find(timeSlice == 0, 1,'first');
+                 iEndMatchZero = find(timeSlice == 0,1,'last');
 
                  if (iStartMatchZero > 1 && iEndMatchZero < 12) % #####------##### (# = val)
-                     limStart = arraymonth(iStartMatchZero-1);
-                     limEnd = arraymonth(iEndMatchZero+1);
+                     limStart = timeSlice(iStartMatchZero-1);
+                     limEnd = timeSlice(iEndMatchZero+1);
                      vectorToInterp = zeros(nValsToInterp+2,1);
                      vectorToInterp(1) = limStart;
                      vectorToInterp(end) = limEnd;
@@ -97,13 +118,13 @@ for iCol = 1:length(columnCoords)
                      tt = 1:nValsToInterp+2;
                      interpVals = interp1(tt(~matchZerosVec),vectorToInterp(~matchZerosVec),...
                          tt(matchZerosVec),'linear','extrap');
-                     arraymonth(iStartMatchZero:iEndMatchZero) = interpVals;
-                     qarray(iRow,iCol,:) = arraymonth; 
+                     timeSlice(iStartMatchZero:iEndMatchZero) = interpVals;
+                     qArray(iRow,iCol,:) = timeSlice; 
                  else
-                     iLimStart = find(arraymonth(:),1,'first');
-                     iLimEnd = find(arraymonth(:),1,'last');
-                     limStart = arraymonth(iLimStart);
-                     limEnd = arraymonth(iLimEnd);
+                     iLimStart = find(timeSlice(:),1,'first');
+                     iLimEnd = find(timeSlice(:),1,'last');
+                     limStart = timeSlice(iLimStart);
+                     limEnd = timeSlice(iLimEnd);
                      vectorToInterp = zeros(nValsToInterp+2,1);
                      vectorToInterp(1) = limStart;
                      vectorToInterp(end) = limEnd;
@@ -114,33 +135,34 @@ for iCol = 1:length(columnCoords)
                      % Three cases
                      if (iLimStart > 1 && iLimEnd < 12) % ------#####-----
                          nValsFirstSec = iLimStart-1;
-                         arraymonth(1:iLimStart-1) = flip(interpVals(1:nValsFirstSec));
-                         arraymonth(iLimEnd+1:12) = flip(interpVals(nValsFirstSec+1:nValsToInterp));
-                         qarray(iRow,iCol,:) = arraymonth; 
+                         timeSlice(1:iLimStart-1) = flip(interpVals(1:nValsFirstSec));
+                         timeSlice(iLimEnd+1:12) = flip(interpVals(nValsFirstSec+1:nValsToInterp));
+                         qArray(iRow,iCol,:) = timeSlice; 
                      elseif (iLimStart == 1 && iLimEnd < 12) % #######--------
-                         arraymonth(iLimEnd+1:12) = flip(interpVals(:)); 
-                         qarray(iRow,iCol,:) = arraymonth; 
+                         timeSlice(iLimEnd+1:12) = flip(interpVals(:)); 
+                         qArray(iRow,iCol,:) = timeSlice; 
                      elseif (iLimStart > 1 && iLimEnd == 12) % -------#######
-                         arraymonth(1:iLimStart-1) = flip(interpVals(:)); 
-                         qarray(iRow,iCol,:) = arraymonth; 
+                         timeSlice(1:iLimStart-1) = flip(interpVals(:)); 
+                         qArray(iRow,iCol,:) = timeSlice; 
                      end
                  end
-                     
+
              else 
-                 
-                 qarray(iRow,iCol,:) = arraymonth;
-                 
+
+                 qArray(iRow,iCol,:) = timeSlice;
+
              end
-             
-         else % sum(arraymonth > 0) <= 1
-         
-            qarray(iRow,iCol,:) = arraymonth;
-         
+
+         else % sum(timeSlice > 0) <= 1
+
+            qArray(iRow,iCol,:) = timeSlice;
+
          end
     end
 end
 
-qarray(qarray<0) = 0;
-qarray(qarray==0) = NaN; % land
+% Post-processing: set negative values to zero and zeros to NaN (land)
+qArray(qArray<0) = 0;
+qArray(qArray==0) = NaN; 
 
-end
+end % cleverTimeInterpolation
