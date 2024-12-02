bb70b681cfbe02bfa9dc0408f82835c28ced7cea
annarufas <128830450+annarufas@users.noreply.github.com>
2024-12-02 17:13:54 +0000

    Update gitignore

--- resources/internal/saveFigure.m
+++
@@ -1,8 +0,0 @@
-function saveFigure(figureName)
-    
-    exportgraphics(gcf,fullfile('.','figures',strcat(figureName,'.pdf')),...
-        'Resolution',600)
-    exportgraphics(gcf,fullfile('.','figures',strcat(figureName,'.png')),...
-        'Resolution',600)  
-
-end
